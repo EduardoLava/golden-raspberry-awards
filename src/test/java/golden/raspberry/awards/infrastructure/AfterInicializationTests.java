ebddae85f61e6a79ea237e6d26f05c23afc41582
Eduardo Lava <eduardo@eduardo.lava@gmail.com>
2022-03-26 18:38:50 -0300

    Testes de integração para validar a inicialização da aplicação

---
+++ src/test/java/golden/raspberry/awards/infrastructure/AfterInicializationTests.java
@@ -0,0 +1,51 @@
+package golden.raspberry.awards.infrastructure;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import golden.raspberry.awards.domain.entity.cine.Movie;
+import golden.raspberry.awards.domain.entity.cine.Producer;
+import golden.raspberry.awards.domain.entity.cine.Studio;
+import golden.raspberry.awards.domain.service.cine.MovieService;
+import golden.raspberry.awards.domain.service.cine.ProducerService;
+import golden.raspberry.awards.domain.service.cine.StudioService;
+
+@SpringBootTest
+class AfterInicializationTests {
+
+	@Autowired
+	MovieService movieService;
+	
+	@Autowired
+	ProducerService producerService;
+	
+	@Autowired
+	StudioService studioService;
+	
+	@Test
+	void checkCsvImportMoviesTestMustPass() {
+		List<Movie> movies = movieService.listAll();
+		
+		assertThat(movies).isNotNull().isNotEmpty();
+	}
+	
+	@Test
+	void checkCsvImportStudiosTestMustPass() {
+		List<Studio> studios = studioService.listAll();
+		
+		assertThat(studios).isNotNull().isNotEmpty();
+	}
+	
+	@Test
+	void checkCsvImportProducerTestMustPass() {
+		List<Producer> producers = producerService.listAll();
+		
+		assertThat(producers).isNotNull().isNotEmpty();
+	}
+	
+}
