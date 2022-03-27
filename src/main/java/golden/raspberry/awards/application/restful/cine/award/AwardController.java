cd99d9c5bb88343ce359d0372c03c357f54d3458
Eduardo Lava <eduardo@eduardo.lava@gmail.com>
2022-03-27 14:01:22 -0300

    Descrição da documentação da API

--- src/main/java/golden/raspberry/awards/application/restful/cine/award/AwardController.java
+++ src/main/java/golden/raspberry/awards/application/restful/cine/award/AwardController.java
@@ -6,13 +6,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import golden.raspberry.awards.domain.dto.cine.award.AwardIntevalDto;
 import golden.raspberry.awards.domain.service.cine.award.AwardService;
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController(value = "/api/awards")
 public class AwardController {
 
 	@Autowired
 	private AwardService awardService;
-	
+
+	@Operation(summary = "Lista os vencedores com maior e menor intervalo "
+						+ "entre dois prêmios do Golden Raspberry Awards")
 	@GetMapping("max-min-intervals")
 	public AwardIntevalDto findMaxMinAwardsIntervals(){
 
